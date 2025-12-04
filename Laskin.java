8000b395cfc2703dd30939c98123fff448b3c97f
luka.vires <luka.vires@edu.keuda.fi>
2025-12-04 09:55:37 +0200

    Lisätty yhteenlasku-metodi

--- Laskin.java
+++ Laskin.java
@@ -1,7 +1,10 @@
 package kfnbdhnbd;
 
 public class Laskin {     
-public static void main(String[] args) {         
-System.out.println("Laskin v1.0");     
-} 
+public static void main(String[] args) { 
+System.out.println("Laskin v1.0");         
+System.out.println("5 + 3 = " + yhteenlasku(5, 3));     
+}          
+public static int yhteenlasku(int a, int b) {         
+return a + b;     } 
 }
