c9ee40890542a8e6961598983d7faac2cb63ea7a
luka.vires <luka.vires@edu.keuda.fi>
2025-12-04 10:01:03 +0200

    Lisätty vähennyslasku-metodi

--- Laskin.java
+++ Laskin.java
@@ -3,8 +3,13 @@ package kfnbdhnbd;
 public class Laskin {     
 public static void main(String[] args) { 
 System.out.println("Laskin v1.0");         
-System.out.println("5 + 3 = " + yhteenlasku(5, 3));     
+System.out.println("5 + 3 = " + yhteenlasku(5, 3));
+System.out.println("10 - 4 = " + vähennyslasku(10, 4));
 }          
+
 public static int yhteenlasku(int a, int b) {         
 return a + b;     } 
+
+public static int vähennyslasku(int a, int b) {     
+return a - b; }
 }
